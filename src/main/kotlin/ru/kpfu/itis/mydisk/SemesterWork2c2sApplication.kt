1ba82fcf2763bb998d93487814b25c37867641cd
BiryukAl <kerzemyak001@gmail.com>
2023-05-04 13:13:08 +0300

    Initial commit

---
+++ src/main/kotlin/ru/kpfu/itis/mydisk/SemesterWork2c2sApplication.kt
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.mydisk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SemesterWork2c2sApplication
+
+fun main(args: Array<String>) {
+	runApplication<SemesterWork2c2sApplication>(*args)
+}
---
+++ src/test/kotlin/ru/kpfu/itis/mydisk/SemesterWork2c2sApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.mydisk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SemesterWork2c2sApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
